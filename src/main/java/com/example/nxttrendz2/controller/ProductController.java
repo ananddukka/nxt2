fd7c40952ba4797b61038fe1d98506b199716ec6
ananddukka <ananddukka83@gmail.com>
2024-04-01 08:33:43 +0000

    solution

---
+++ src/main/java/com/example/nxttrendz2/controller/ProductController.java
@@ -0,0 +1,46 @@
+package com.example.nxttrendz2.controller;
+
+import com.example.nxttrendz2.model.Category;
+import com.example.nxttrendz2.model.Product;
+import com.example.nxttrendz2.service.ProductJpaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+
+@RestController
+public class ProductController {
+    @Autowired
+    private ProductJpaService productJpaService;
+
+    @GetMapping("/categories/products")
+    public ArrayList<Product> getProducts(){
+        return productJpaService..getProducts();
+    }
+
+    @GetMapping("/categories/products/{productId}")
+    public Product getProductById(@PathVariaable("productId") int productId) {
+        return productJpaService.getProductById(productId);
+    }
+
+    @PostMapping("/categories/products")
+    public Product addProduct(@RequestBody Product product) {
+        return productJpaService.addProduct(product);
+    }
+
+    @PutMapping("/categories/products/{productId}")
+    public Product updateProduct(@PathVariable("productId") int productId, @RequestBody Product){
+
+     return productJpaService.addProduct(product);
+}
+@DeleteMapping("/categories/products/{productId}")
+public void deleteProduct(@PathVariable("productId") int productId){
+    productJpaService.deleteProduct(productId);
+}
+
+@GetMapping("/products/{productId}/category")
+public Category getProductCategory(@PathVariable("productId") int productId){
+    return productJpaService.getProductCategory(productId);
+}
+
+}
+
